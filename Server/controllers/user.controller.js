4599a57bfd664717045384613bdb37df3cbec38c
PUCKCODERS <jonitaweb.jobs@gmail.com>
2025-07-29 14:32:12 -0500

    antes de modificar los correos/ funciona todo bien con mongo

---
+++ Server/controllers/user.controller.js
@@ -0,0 +1,74 @@
+import UserModel from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import sendEmailFun from "../config/sendEmail.js";
+import VerificationEmail from "../utils/verifyEmailTemplate.js";
+
+export async function registerUserController(request, response) {
+  try {
+    let user;
+
+    const { name, email, password } = request.body;
+    if (!name || !email || !password) {
+      return response.status(400).json({
+        message: "PROPORCIONAR CORREO ELECTRÓNICO, NOMBRE, CONTRASEÑA",
+        error: true,
+        success: false,
+      });
+    }
+
+    user = await UserModel.findOne({ email: email });
+    if (user) {
+      return response.json({
+        message: "USUARIO YA REGISTRADO CON ES EMAIL",
+        error: true,
+        success: false,
+      });
+    }
+
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashPassword = await bcryptjs.hash(password, salt);
+
+    user = new UserModel({
+      email: email,
+      password: hashPassword,
+      name: name,
+      otp: verifyCode,
+      otpExpires: Date.now() + 600000,
+    });
+
+    await user.save();
+
+    // Send verification email
+    await sendEmailFun({
+      sendTo: email,
+      subject: "VERIFICAR CORREO ELECTRÓNICO DESDE LA APLICACION DE MAQUITEXT",
+      text: "",
+      html: VerificationEmail(name, verifyCode),
+    });
+
+    const token = jwt.sign(
+      {
+        email: user.email,
+        id: user._id,
+      },
+      process.env.JSON_WEB_TOKEN_SECRET_KEY
+    );
+
+    return response.status(200).json({
+      success: true,
+      error: false,
+      message:
+        "USUARIO REGISTRADO CORRECTAMENTE! POR FAVOR, REVISE SU CORREO ELECTRÓNICO",
+      token: token,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
