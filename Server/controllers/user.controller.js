4ebb6a8f9a78283f7857ab185d157fec07935dc3
PUCKCODERS <jonitaweb.jobs@gmail.com>
2025-07-31 11:40:32 -0500

    Login y Logout creados

--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -3,6 +3,8 @@ import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import sendEmailFun from "../config/sendEmail.js";
 import VerificationEmail from "../utils/verifyEmailTemplate.js";
+import generatedAccessToken from "../utils/generatedAccessToken.js";
+import generatedRefreshToken from "../utils/generatedRefreshToken.js";
 
 export async function registerUserController(request, response) {
   try {
@@ -63,6 +65,146 @@ export async function registerUserController(request, response) {
       message:
         "USUARIO REGISTRADO CORRECTAMENTE! POR FAVOR, REVISE SU CORREO ELECTRÓNICO",
       token: token,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+export async function verifyEmailController(request, response) {
+  try {
+    const { email, otp } = request.body;
+
+    const user = await UserModel.findOne({ email: email });
+    if (!user) {
+      return response.status(400).json({
+        error: true,
+        success: false,
+        message: "USUARIO NO ENCONTRADO",
+      });
+    }
+
+    const isCodeValid = user.otp === otp;
+    const isNotExpired = user.otpExpires > Date.now();
+
+    if (isCodeValid && isNotExpired) {
+      user.verify_email = true;
+      user.otp = null;
+      user.otpExpires = null;
+      await user.save();
+      return response.status(200).json({
+        error: false,
+        success: true,
+        message: "CORREO ELECTRÓNICO VERIFICADO EXITOSAMENTE",
+      });
+    } else if (!isCodeValid) {
+      return response
+        .status(400)
+        .json({ error: true, success: false, message: "CODIGO NO VÁLIDO" });
+    } else {
+      return response
+        .status(400)
+        .json({ error: true, success: false, message: "CODIGO CADUCADO" });
+    }
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: true,
+    });
+  }
+}
+
+export async function loginUserController(request, response) {
+  try {
+    const { email, password } = request.body;
+
+    const user = await UserModel.findOne({ email: email });
+
+    if (!user) {
+      response.status(400).json({
+        message: "USUARIO NO REGISTRADO",
+        error: true,
+        success: false,
+      });
+    }
+
+    if (user.status !== "Active") {
+      return response.status(400).json({
+        message: "CONTACTE CON ADMINISTRADOR",
+        error: true,
+        success: false,
+      });
+    }
+
+    const checkPassword = await bcryptjs.compare(password, user.password);
+
+    if (!checkPassword) {
+      return response.status(400).json({
+        message: "COMPRUEBA TU CONTRASEÑA",
+        error: true,
+        success: false,
+      });
+    }
+
+    const accesstoken = await generatedAccessToken(user._id);
+    const refreshToken = await generatedRefreshToken(user._id);
+
+    const updateUser = await UserModel.findByIdAndUpdate(user?._id, {
+      last_login_date: new Date(),
+    });
+
+    const cookiesOption = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    };
+    response.cookie("accessToken", accesstoken, cookiesOption);
+    response.cookie("refreshToken", refreshToken, cookiesOption);
+
+    return response.json({
+      message: "INICIO DE SESIÓN EXITOSAMENTE",
+      error: false,
+      success: true,
+      data: {
+        accesstoken,
+        refreshToken,
+      },
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+export async function logoutController(request, response) {
+  try {
+    const userid = request.userid;
+
+    const cookiesOption = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    };
+
+    response.clearCookie("accessToken", cookiesOption);
+    response.clearCookie("refreshToken", cookiesOption);
+
+    const removeRefreshToken = await UserModel.findByIdAndUpdate(userid, {
+      refresh_token: "",
+    });
+
+    return response.json({
+      message: "SESIÓN CERRADA EXITOSAMENTE",
+      error: false,
+      success: true,
     });
   } catch (error) {
     return response.status(500).json({
