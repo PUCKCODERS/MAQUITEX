39d700d11c302eec451324f52d0f5df1a5013455
PUCKCODERS <export async function verifyForgotPasswordOtp(request, response) {>
2025-08-15 06:22:46 -0500

    cartproducts antes de editar

--- Server/controllers/cart.controller.js
+++ Server/controllers/cart.controller.js
@@ -52,3 +52,117 @@ export const addToCartItemController = async (request, response) => {
     });
   }
 };
+
+export const getCartItemController = async (request, response) => {
+  try {
+    const userId = request.userId;
+
+    const cartItem = await CartProductModel.find({
+      userId: userId,
+    }).populate("productId");
+
+    return response.json({
+      data: cartItem,
+      error: false,
+      success: true,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+};
+
+export const updateCartItemQtyController = async (request, response) => {
+  try {
+    const userId = request.userId;
+    const { _id, qty } = request.body;
+
+    if (!_id || !qty) {
+      return response.status(400).json({
+        message: "PROPORCIONAR _id, qty",
+      });
+    }
+
+    const updateCartitem = await CartProductModel.updateOne(
+      {
+        _id: _id,
+        userId: userId,
+      },
+      {
+        quantity: qty,
+      }
+    );
+
+    return response.json({
+      message: "ACTUALIZAR CARRITO",
+      success: true,
+      error: false,
+      data: updateCartitem,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+};
+
+export const deleteCartItemQtyController = async (request, response) => {
+  try {
+    const userId = request.userId;
+    const { _id, productId } = request.body;
+
+    if (!_id) {
+      return response.status(400).json({
+        message: "PROPORCIONAR _id",
+        error: true,
+        success: false,
+      });
+    }
+
+    const deleteCartItem = await CartProductModel.deleteOne({
+      _id: _id,
+      userId: userId,
+    });
+
+    if (!deleteCartItem) {
+      return response.status(404).json({
+        message: "NO SE ENCUENTRA EL PRODUCTO EN EL CARRITO",
+        error: true,
+        success: false,
+      });
+    }
+
+    const user = await UserModel.findOne({
+      _id: userId,
+    });
+
+    const cartItems = user?.shopping_cart;
+
+    const updatedUserCart = [
+      ...cartItems.slice(0, cartItems.indexOf(productId)),
+      ...cartItems.slice(cartItems.indexOf(productId) + 1),
+    ];
+
+    user.shopping_cart = updatedUserCart;
+
+    await user.save;
+
+    return response.json({
+      message: "ITEM ELIMINADO",
+      error: false,
+      success: true,
+      data: deleteCartItem,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+};
