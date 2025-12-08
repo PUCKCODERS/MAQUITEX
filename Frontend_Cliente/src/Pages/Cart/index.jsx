0a8a3f06a6e7a02b0c4dd36f65696cfc85630969
PUCKCODERS <jonitaweb.jobs@gmail.com>
2025-12-07 21:53:04 -0500

    Carrito vacio editado

--- Frontend_Cliente/src/Pages/Cart/index.jsx
+++ Frontend_Cliente/src/Pages/Cart/index.jsx
@@ -3,6 +3,7 @@ import Button from "@mui/material/Button";
 import { FaCashRegister } from "react-icons/fa6";
 import CartItems from "./cartItems";
 import { MyContext } from "../../App";
+import { GiShoppingCart } from "react-icons/gi";
 
 const CartPage = () => {
   const context = useContext(MyContext);
@@ -12,32 +13,72 @@ const CartPage = () => {
       <div className="container w-[80%] max-w-[80%] flex !gap-5">
         <div className="leftPart w-[70%]">
           <div className="shadow-md rounded-md  bg-white">
-            <div className="!py-2 !px-3 border-b border-[#d1d1d1] ">
-              <h2 className="font-[bold]  !text-[25px] flex justify-center">
-                TU CARRITO
-              </h2>
-              <p className="!mt-2 !mb-2 text-[#556f8d] !text-[20px] font-[600] flex justify-center">
-                ESTOS SON
-                <span className="font-bold text-[#ec370a]">
-                  &nbsp;{context?.cartData?.length}&nbsp;
-                </span>
-                PRODUCTOS EN TU CARRITO
-              </p>
-            </div>
+            {context?.cartData?.length === 0 ? (
+              <div className="!py-10 !px-3 flex items-center justify-center">
+                <div className="!mt-3 flex flex-col justify-center items-center">
+                  <span className="font-bold !text-[#082c55] !text-[20px] ">
+                    CARRITO VACÍO
+                  </span>
 
-            {context?.cartData?.length !== 0 &&
-              context?.cartData.map((item, index) => {
-                return (
-                  <CartItems
-                    item={item}
-                    key={index}
-                    size={item.size}
-                    ram={item.ram}
-                    weight={item.weight}
-                    quantity={item?.quantity}
-                  />
-                );
-              })}
+                  <GiShoppingCart className="font-bold !text-[#082c55] !text-[90px] " />
+
+                  <div className="text-center !mt-4 !max-w-xs !p-2">
+                    <p className="text-gray-950 !mb-3">
+                      ¡Parece que aún no has agregado nada! Explora nuestras
+                      categorías y encuentra ese producto que tanto deseas.
+                    </p>
+
+                    <button className="bg-[#082c55] text-white font-semibold !py-2 !px-4 rounded transition duration-300 shadow-md">
+                      CONTINUAR COMPARNDO Y VER OFERTAS
+                    </button>
+
+                    <div className="!mt-3 text-sm">
+                      <a
+                        href="/favoritos"
+                        className="text-blue-600 hover:text-blue-800 !mx-2"
+                      >
+                        IR A MIS FAVORITOS
+                      </a>
+                      <span className="text-gray-400">|</span>
+                      <a
+                        href="/vistos-recientemente"
+                        className="text-blue-600 hover:text-blue-800 !mx-1"
+                      >
+                        COMPARTIDOS
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <>
+                <div className="!py-2 !px-3 border-b border-[#d1d1d1] ">
+                  <h2 className="font-[bold]  !text-[25px] flex justify-center">
+                    TU CARRITO
+                  </h2>
+                  <p className="!mt-2 !mb-2 text-[#556f8d] !text-[20px] font-[600] flex justify-center">
+                    ESTOS SON
+                    <span className="font-bold text-[#ec370a]">
+                      &nbsp;{context?.cartData?.length}&nbsp;
+                    </span>
+                    PRODUCTOS EN TU CARRITO
+                  </p>
+                </div>
+
+                {context?.cartData.map((item, index) => {
+                  return (
+                    <CartItems
+                      item={item}
+                      key={index}
+                      size={item.size}
+                      ram={item.ram}
+                      weight={item.weight}
+                      quantity={item?.quantity}
+                    />
+                  );
+                })}
+              </>
+            )}
           </div>
         </div>
 
