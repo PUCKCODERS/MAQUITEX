fa343046a0bccc8ffb594894290a9a67b5d35819
PUCKCODERS <jonitaweb.jobs@gmail.com>
2026-02-09 20:00:58 -0500

    MobileNav

--- Frontend_Cliente/src/components/Header/Navigation/mobileNav.jsx
+++ Frontend_Cliente/src/components/Header/Navigation/mobileNav.jsx
@@ -1,37 +1,43 @@
 import { Button } from "@mui/material";
 import React from "react";
-import { SiHomeassistantcommunitystore } from "react-icons/si";
 import { BsFilePersonFill } from "react-icons/bs";
-import { FaHeart } from "react-icons/fa";
-import { FaShoppingCart } from "react-icons/fa";
-import { ImSearch } from "react-icons/im";
+import { FcLike } from "react-icons/fc";
+import { GiShoppingCart } from "react-icons/gi";
+import { FcSearch } from "react-icons/fc";
 import { NavLink, useLocation } from "react-router-dom";
+import { FcShop } from "react-icons/fc";
+import { FcFilledFilter } from "react-icons/fc";
 
 const MobileNav = () => {
   const location = useLocation();
 
   return (
-    <div className="mobileNav bg-white !p-1 !px-3 w-full grid grid-cols-5 fixed !bottom-0 !left-0 place-items-center !gap-5 z-[51]">
+    <div className="mobileNav bg-white !p-1 !px-3 !w-full fixed !bottom-0 !left-0 flex items-center justify-between !gap-0 z-[51] border-t-1">
       <NavLink to="/" exact={true} activeClassName="isActive">
         <Button
           className={`flex-col !w-[40px] !min-w-[40px] !capitalize ${location.pathname === "/" ? "!text-[#143253]" : "!text-gray-500"}`}
         >
-          <SiHomeassistantcommunitystore size={18} />
-          <span className="text-[12px] ">INICIO</span>
+          <FcShop size={25} />
+          <span className="text-[10px] lg:text-[12px] ">INICIO</span>
         </Button>
       </NavLink>
 
-      <Button className="flex-col !w-[40px] !min-w-[40px] !capitalize !text-gray-700">
-        <ImSearch size={18} />
-        <span className="text-[12px]">BUSCAR</span>
+      <Button className="flex-col !w-[40px] !min-w-[40px] !capitalize !text-gray-500">
+        <FcFilledFilter size={25} />
+        <span className="text-[10px] lg:text-[12px]">FILTROS</span>
+      </Button>
+
+      <Button className="flex-col !w-[40px] !min-w-[40px] !capitalize !text-gray-500">
+        <FcSearch size={25} />
+        <span className="text-[10px] lg:text-[12px]">BUSCAR</span>
       </Button>
 
       <NavLink to="/my-list" exact={true} activeClassName="isActive">
         <Button
           className={`flex-col !w-[40px] !min-w-[40px] !capitalize ${location.pathname === "/my-list" ? "!text-[#143253]" : "!text-gray-500"}`}
         >
-          <FaHeart size={18} />
-          <span className="text-[12px]">GUSTA</span>
+          <FcLike size={25} />
+          <span className="text-[10px] lg:text-[12px]">GUSTA</span>
         </Button>
       </NavLink>
 
@@ -39,8 +45,8 @@ const MobileNav = () => {
         <Button
           className={`flex-col !w-[40px] !min-w-[40px] !capitalize ${location.pathname === "/my-orders" ? "!text-[#143253]" : "!text-gray-500"}`}
         >
-          <FaShoppingCart size={18} />
-          <span className="text-[12px]">PEDIDOS</span>
+          <GiShoppingCart size={25} color="#111827" />
+          <span className="text-[10px] lg:text-[12px]">PEDIDOS</span>
         </Button>
       </NavLink>
 
@@ -48,8 +54,8 @@ const MobileNav = () => {
         <Button
           className={`flex-col !w-[40px] !min-w-[40px] !capitalize ${location.pathname === "/my-account" ? "!text-[#143253]" : "!text-gray-500"}`}
         >
-          <BsFilePersonFill size={18} />
-          <span className="text-[12px]">PERFIL</span>
+          <BsFilePersonFill size={25} color="#16a34a" />
+          <span className="text-[10px] lg:text-[12px]">PERFIL</span>
         </Button>
       </NavLink>
     </div>
