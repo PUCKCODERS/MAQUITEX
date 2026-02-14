cd0e2c0ab0d2528b94901e767d6c930470f133d4
PUCKCODERS <jonitaweb.jobs@gmail.com>
2026-02-14 00:40:37 -0500

    success y failed

--- Frontend_Cliente/src/Pages/Orders/failed.jsx
+++ Frontend_Cliente/src/Pages/Orders/failed.jsx
@@ -5,10 +5,14 @@ import { Link } from "react-router-dom";
 const OrderFailed = () => {
   window.scrollTo(0, 0);
   return (
-    <section className="w-full !p-10 !py-20 flex items-center justify-center flex-col !gap-2">
+    <section className="w-full !p-10 !py-5 sm:!py-10 md:!py-15 lg:!py-20 flex items-center justify-center flex-col !gap-2">
       <img src="../../../imagenes/orderFalied.png" width="200" />
-      <h3 className="!mb-0 text-[25]">SU PEDIDO HA FALLADO</h3>
-      <p className="!mt-0">SU PEDIDO FALLÓ DEBIDO A ALGUNA RAZÓN</p>
+      <h3 className="!mt-0 text-center !text-[15px] sm:!text-[20px] md:!text-[25px] lg:!text-[25px] !text-gray-600">
+        SU PEDIDO HA FALLADO
+      </h3>
+      <p className="!mt-0 text-center !text-[15px] sm:!text-[20px] md:!text-[25px] lg:!text-[25px] !text-gray-500">
+        SU PEDIDO FALLÓ DEBIDO A ALGUNA RAZÓN
+      </p>
       <Link to="/">
         <Button className="btn-org btn-border">REGRESAR AL INICIO</Button>
       </Link>
